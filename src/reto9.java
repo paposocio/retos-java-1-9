5077e599573cd29bc185e2f4f5a6e6d4ce5760b7
sap0 <paposocio322@gmail.com>
2023-03-01 07:46:56 -0500

    reto 9 funcional

--- src/reto9.java
+++ src/reto9.java
@@ -1,56 +1,54 @@
 import java.util.Scanner;
 public class reto9 {
     public static void main(String[] args) {
-        Scanner lectura=new Scanner (System.in);
+        Scanner lectura =new Scanner (System.in);
 
         int bet = 0;
         int attempts = 1;
-        int counter = 0;
 
-        for (counter=0;counter <= 2; counter++)
-        {
-            System.out.println("Bienvenid@ al juego del cara y sello por favor ingrese un monto para apostar");
-            int userBet =lectura.nextInt();
-            
-            System.out.println("Seleccione una opcion (1=cara/2=sello)");
-            int coin =lectura.nextInt();
+            for ( int counter=0;counter <= 2; counter++)
+            {
+                System.out.println("Bienvenid@ al juego del cara y sello por favor ingrese un monto para apostar");
+                int userBet =lectura.nextInt();
+                
+                System.out.println("Seleccione una opcion (1=cara/2=sello)");
+                int coin =lectura.nextInt();
 
-            System.out.println("*Girando*");
-            int result = (int) (Math.random() * (2-1+1) ) + 1;
-    
-            if (result==coin && coin ==1)
-            {
-                System.out.println("*cara* Haz ganado");
-                int money=userBet*2;
-                bet += money;
-            }
-            else if (result==coin && coin ==2)
-            {
-                System.out.println("*sello* Haz ganado");
-                int money=userBet*2;
-                bet += money;
-            }
-            else 
-            {
-                System.out.println("Perdiste , intentalo de nuevo");
-                bet -= userBet;
-            }
+                System.out.println("*Girando*");
+                int result = (int) (Math.random() * (2-1+1) ) + 1;
+        
+                    if (result==coin && coin ==1)
+                    {
+                        System.out.println("*cara* Haz ganado");
+                        int money=userBet*2;
+                        bet += money;
+                    }
+                    else if (result==coin && coin ==2)
+                    {
+                        System.out.println("*sello* Haz ganado");
+                        int money=userBet*2;
+                        bet += money;
+                    }
+                    else 
+                    {
+                        System.out.println("Perdiste , intentalo de nuevo");
+                        bet -= userBet;
+                    }
 
-            System.out.println("Desea realizar otro intento?? (1=si/2=no)");
-            int option =lectura.nextInt();
+                System.out.println("Desea realizar otro intento?? (1=si/2=no)");
+                int option =lectura.nextInt();
 
-            if (option == 2)
-            {
-                counter = 2;
-                System.out.println("Usted jugo un total de: "+attempts+" intentos y acumulo un total de "+bet);
+                    if (option == 2)
+                    {
+                        counter = 2;
+                        System.out.println("Usted jugo un total de: "+attempts+" intentos y acumulo un total de "+bet);
+                    }
+                    else if (option== 1)
+                    {
+                        counter = 1;
+                        attempts ++;
+                    }
             }
-            else if (option== 1)
-            {
-                counter = 1;
-                attempts ++;
-            }
-
-        }
 
         lectura.close();
     }
